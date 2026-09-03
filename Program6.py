c6dd4a95d861daa82fd9faa56e74267d48db49bf
Pooja Meena <poojameena777xyz@gmail.com>
2026-09-03 12:01:26 +0530

    added Program6

---
+++ Program6.py
@@ -0,0 +1,12 @@
+d = {
+    "name": "Pooja",
+    "city": "Coimbatore",
+    "course": "Python"
+}
+
+for key, value in d.items():
+    print("Key:", key)
+    print("Uppercase:", value.upper())
+    print("Lowercase:", value.lower())
+    print("Length:", len(value))
+    print()
